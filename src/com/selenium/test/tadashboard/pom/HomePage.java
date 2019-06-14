a0e4a243dec4b553ce31e96111229863d6be79b5
nghieptruong <nghieptruong89@gmail.com>
2019-06-14 23:49:10 +0700

    update HomePage - adding NavigationHeaderMenu

--- src/com/selenium/test/tadashboard/pom/HomePage.java
+++ src/com/selenium/test/tadashboard/pom/HomePage.java
@@ -10,57 +10,33 @@ import com.selenium.testfw.common.*;
 
 public class HomePage extends BasePage {
 	
-	private final String repoXpathFormat = "//ul[@id='ulListRepositories']/li/a[.='%s']";
-
-	By imgIconLoading = By.xpath("//img[@src='images/loading.gif']");
+	// Elements
+	private final By imgIconLoading = By.xpath("//img[@src='images/loading.gif']");
 	
-	@FindBy(xpath = "//a[text()='Repository: ']/span")
-	private WebElement txtRepoName;
+	// Page Components
+	private final NavigationHeaderMenu navigationHeaderMenu;
 	
-	@FindBy(xpath = "//a[@href='#Welcome']")
-	private WebElement lnkWelcome;
 	
-	@FindBy(xpath = "//a[contains(.,'Repository: ')]")
-	private WebElement lnkRepository;
-	
-	public HomePage(WebDriver driver) {
+	public HomePage(WebDriver driver) throws Exception {
 		super(driver);
+		navigationHeaderMenu = GetInstance(NavigationHeaderMenu.class);
 	}
 
-	public String getRepoName() throws Exception {
-		waitForDisplay(txtRepoName, 20);
-		return getText(txtRepoName);		
+	public NavigationHeaderMenu navigationMenu() {
+		return navigationHeaderMenu;
 	}
 	
 	public void checkRepoDisplay(String repoName) throws Exception {
-		Assert.assertEquals(getRepoName(), repoName, "Repo is incorrect");
-	}
-	
-	public String getUserLogin() throws Exception {
-		waitForDisplay(lnkWelcome, 20);
-		return getText(lnkWelcome);
+		Assert.assertEquals(navigationMenu().getRepoName(), repoName, "Repo is incorrect");
 	}
 	
 	public void checkLoginUserDisplay(String userName) throws Exception {
-		Assert.assertEquals(getUserLogin(), userName);
+		Assert.assertEquals(navigationMenu().getUserLogin(), userName);
 	}
 	
-	public void waitForHomePageReady(int timeOutInSeconds) throws Exception {
+	public void waitForIconLoadingNotDisplay(int timeOutInSeconds) throws Exception {
 		waitForAllInvisibility(imgIconLoading, timeOutInSeconds);
 	}
 	
-	public LoginPage logout() throws Exception {
-		String userLogin = getUserLogin();
-		String menuItem = userLogin + "->" + "Logout";
-		selectMenuItem(menuItem, Constant.ELEMENT_TIMEOUT);
-		return (LoginPage) GetInstance(LoginPage.class).waitForPageLoaded();
-	}
 	
-	public HomePage selectRepo(String repo) throws Exception {
-		String xpathRepo = String.format(repoXpathFormat, repo);
-		click(lnkRepository);
-		click(waitForDisplay(By.xpath(xpathRepo), Constant.ELEMENT_TIMEOUT));
-		waitForHomePageReady(Constant.ELEMENT_TIMEOUT + 60);
-		return this;
-	}
 }

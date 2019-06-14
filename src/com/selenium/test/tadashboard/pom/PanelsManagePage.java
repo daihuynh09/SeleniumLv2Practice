54cb7ef80772681f0311c90ae5b014efdb8b9174
nghieptruong <nghieptruong89@gmail.com>
2019-06-14 23:47:03 +0700

    create PanelsManagePage page

---
+++ src/com/selenium/test/tadashboard/pom/PanelsManagePage.java
@@ -0,0 +1,14 @@
+package com.selenium.test.tadashboard.pom;
+
+import org.openqa.selenium.WebDriver;
+
+import com.selenium.testfw.common.BasePage;
+
+public class PanelsManagePage extends BasePage {
+
+	public PanelsManagePage(WebDriver driver) {
+		super(driver);
+	}
+
+	//TO-DO...
+}

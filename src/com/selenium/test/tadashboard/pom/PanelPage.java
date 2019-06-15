dd2ea2341d94a1e087bdfe87576ad5a3f4199be8
nghieptruong <nghieptruong89@gmail.com>
2019-06-15 10:33:15 +0700

    create PanelPage

---
+++ src/com/selenium/test/tadashboard/pom/PanelPage.java
@@ -0,0 +1,13 @@
+package com.selenium.test.tadashboard.pom;
+
+import org.openqa.selenium.WebDriver;
+
+import com.selenium.testfw.common.BasePage;
+
+public class PanelPage extends BasePage {
+
+	public PanelPage(WebDriver driver) {
+		super(driver);
+	}
+
+}
